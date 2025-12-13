42b588f974741f99cdb554ee43409a9fb240cb0e
Julien Lisita <julien.lisita@gmail.com>
2025-12-13 15:28:08 +0100

    chore(home): clean up unused sections

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,27 +1,11 @@
 // src/components/pages/Home.tsx
 
-import AboutTeaser from '../home/AboutTeaser';
 import Hero from '../home/Hero';
-import NewsTeaserServer from '../home/NewsTeaserServer';
-import ServicesTeaser from '../home/ServicesTeaser';
-import TestimonialsTeaserServer from '../home/TestimonialsTeaserServer';
-import Cta from '../patterns/Cta';
 
 export default function Home() {
   return (
     <div>
       <Hero />
-      <AboutTeaser />
-      <ServicesTeaser />
-      <TestimonialsTeaserServer />
-      <NewsTeaserServer />
-      <Cta
-        title="Prêt à en savoir plus ?"
-        description="Contactez-nous dès aujourd’hui pour découvrir comment nous pouvons vous accompagner."
-        align="left"
-        primaryLabel="Nous contacter"
-        primaryHref="/contact"
-      />
     </div>
   );
 }
