15dd4268116557bc5b70b439cc6d23b54e6209a7
Julien Lisita <julien.lisita@gmail.com>
2026-01-04 12:40:06 +0100

    feat(home): assemble homepage sections

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,13 +1,21 @@
 // src/components/pages/Home.tsx
 
+import AboutTeaser from '../home/AboutTeaser';
 import BestOfPhotosSection from '../home/BestOfPhotosSection';
+import ExploreSection from '../home/ExploreSection';
+import FinalCtaHome from '../home/FinalCtaHome';
 import Hero from '../home/Hero';
+import JournalTeaser from '../home/JournalTeaser';
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <BestOfPhotosSection />
+      <ExploreSection />
+      <JournalTeaser />
+      <AboutTeaser />
+      <FinalCtaHome />
     </div>
   );
 }
